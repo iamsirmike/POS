116df9488d9c465cd2f7c894c6740850e65d1c5e
MICHAEL ASAMOAH <you@example.com>
2019-08-11 21:44:34 +0000

    new POS

--- connect.php
+++ connect.php
@@ -1,25 +1,25 @@
 <?php
 
 // /* Database config */
-// $db_host = 'localhost';
-// $db_user = 'root';
-// $db_pass = '';
-// $db_database = 'posales';
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+$db_database = 'posales';
 
-// $db = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_database, $db_user, $db_pass);
-// $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_database, $db_user, $db_pass);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //end mySql
 
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"], 1);
+// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $cleardb_server = $cleardb_url["host"];
+// $cleardb_username = $cleardb_url["user"];
+// $cleardb_password = $cleardb_url["pass"];
+// $cleardb_db = substr($cleardb_url["path"], 1);
 
 
-$active_group = 'default';
-$query_builder = true;
+// $active_group = 'default';
+// $query_builder = true;
 
 // $db = new PDO('mysql:host=' . $cleardb_server . ';dbname=' . $cleardb_db, $cleardb_username, $cleardb_password);
 // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -27,7 +27,7 @@ $query_builder = true;
 
 /* End config */
 
-$conn = mysqli_connect("$cleardb_server", "$cleardb_username", "$cleardb_password", "$cleardb_db");
+$conn = mysqli_connect("localhost", "root", "", "posales");
 if (mysqli_connect_errno())//function to check database connection
 {
     die("connection failed") . mysqli_connect_errno();
--- login.php
+++ login.php
@@ -10,26 +10,26 @@ $errmsg_arr = array();
 $errflag = false;
 
 /**Connect to heroku clearDB */
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"], 1);
+// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $cleardb_server = $cleardb_url["host"];
+// $cleardb_username = $cleardb_url["user"];
+// $cleardb_password = $cleardb_url["pass"];
+// $cleardb_db = substr($cleardb_url["path"], 1);
 
 
-$active_group = 'default';
-$query_builder = true;
+// $active_group = 'default';
+// $query_builder = true;
 
-$db = mysqli_connect("$cleardb_server", "$cleardb_username", "$cleardb_password", "$cleardb_db");
+// $db = mysqli_connect("$cleardb_server", "$cleardb_username", "$cleardb_password", "$cleardb_db");
 /**End heroku clearDB */
 
 /**connect to mysql */
-// $db_host = 'localhost';
-// $db_user = 'root';
-// $db_pass = '';
-// $db_database = 'posales';
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+$db_database = 'posales';
 
-// $db = mysqli_connect("$db_host", "$db_user", "$db_pass", "$db_database");
+$db = mysqli_connect("$db_host", "$db_user", "$db_pass", "$db_database");
 /**End mysql */
 if (!$db) {
 	die('Failed to connect to server: ' . mysqli_error());
