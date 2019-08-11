0c126710770bc1778bf5c76b55b2f64fb9adb76c
MICHAEL ASAMOAH <you@example.com>
2019-08-11 21:36:35 +0000

    new POS

--- connect.php
+++ connect.php
@@ -21,8 +21,8 @@ $cleardb_db = substr($cleardb_url["path"], 1);
 $active_group = 'default';
 $query_builder = true;
 
-$db = new PDO('mysql:host=' . $cleardb_server . ';dbname=' . $cleardb_db, $cleardb_username, $cleardb_password);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// $db = new PDO('mysql:host=' . $cleardb_server . ';dbname=' . $cleardb_db, $cleardb_username, $cleardb_password);
+// $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //end heroku clearDB
 
 /* End config */
